e2aae822ec47a49418fc2d1960217f46aab03d1c
patchthesock <stephencallaghan91@gmail.com>
2017-01-20 21:27:37 +0000

    Did Stuff.

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class CameraManager : MonoBehaviour
+    {
+        [HideInInspector]
+        public static CameraManager instance = null;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(gameObject);
+
+            DontDestroyOnLoad(gameObject);
+            InitGame();
+        }
+
+        private void InitGame()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    public class GameManager : MonoBehaviour
+    {
+        public Button PlayBtn;
+
+        [HideInInspector]
+        public static GameManager instance = null;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(gameObject);
+
+            DontDestroyOnLoad(gameObject);
+            _isPlaying = false;
+            InitGame();
+        }
+
+        private void InitGame()
+        {
+            // Play Button Control
+            if (PlayBtn != null)
+            {
+                PlayBtn.onClick.AddListener(() =>
+                {
+                    PlayControl(!_isPlaying);
+                });
+            }
+        }
+
+        private void Update()
+        {
+            Debug.Log(_isPlaying);
+        }
+
+        private void PlayControl(bool state)
+        {
+            Physics.gravity = state ? new Vector3(0, -50, 0) : new Vector3(0, 0, 0);
+            _isPlaying = !_isPlaying;
+        }
+
+        private bool _isPlaying;
+    }
+}
