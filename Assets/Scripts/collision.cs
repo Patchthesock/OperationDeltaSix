cc80c2d26ef1502afe270b3ff6973b131a2c582f
BezleySnipes <David.Mulhall@gmail.com>
2017-01-21 16:20:22 +0000

    Daves stuff

---
+++ Assets/Scripts/collision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collision : MonoBehaviour
+{
+	public AudioClip[] audioFiles;
+	int index;
+	private AudioSource _audioSource;
+
+	void Awake()
+	{
+		_audioSource = GetComponent<AudioSource>();
+	}
+
+
+    void OnCollisionEnter (Collision col)
+    {
+    	
+        if(col.gameObject.tag == "domino")
+        {
+        	index = Random.Range (0, audioFiles.Length);
+   			_audioSource.clip = audioFiles[index];
+            _audioSource.Play();
+        }
+    }
+}
