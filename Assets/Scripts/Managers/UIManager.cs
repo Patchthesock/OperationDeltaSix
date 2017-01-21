5a4a5d60e3a7f37daa193b835c501111ec9dc29b
conorbrien <conorbrien@gmail.com>
2017-01-21 03:10:02 +0000

    UI found

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+	public void HandleClick()
+	{
+		if (this.name == "DominoesButton")
+		{
+			Debug.Log("DominoesButton");
+		}
+	}
+
+}
