26e2f0b7dfe9f4a23efc386b0f98782acf3a5550
conorbrien <conorbrien@gmail.com>
2017-01-21 14:17:08 +0000

    Basic UIScript Functionality

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,15 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-	public void HandleClick()
+	public GameObject DominoesInventory;
+	public GameObject PropsInventory;
+	public GameObject MainUI;
+	public bool InventoryOpen;
+	
+	private GameObject ActiveInventory;
+	
+	public void DominoesButton()
 	{
-		if (this.name == "DominoesButton")
+		DominoesInventory.SetActive(true);
+		ActiveInventory = DominoesInventory;
+		foreach(Transform child in MainUI.transform)
 		{
-			Debug.Log("DominoesButton");
+			child.GetComponent<Button>().interactable = false;
 		}
+		InventoryOpen = true;
+	}
+	
+	public void PropsButton()
+	{
+		PropsInventory.SetActive(true);
+		ActiveInventory = PropsInventory;
+		foreach(Transform child in MainUI.transform)
+		{
+			child.GetComponent<Button>().interactable = false;
+		}
+		InventoryOpen = true;
 	}
 
+	
+	void Update()
+	{
+		if (InventoryOpen == true)
+		{
+			if(Input.GetKeyUp(KeyCode.Escape))
+			{
+				foreach(Transform child in MainUI.transform)
+				{
+					child.GetComponent<Button>().interactable = true;
+					InventoryOpen = false;
+					ActiveInventory.SetActive(false);
+				}
+			}
+		}
+	}	
 }
