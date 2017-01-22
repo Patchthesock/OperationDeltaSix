d9af880123cfeaeeb7a0ee816b76fa825c6b5adc
conorbrien <conorbrien@gmail.com>
2017-01-22 05:54:39 +0000

    Working NewGame

--- Assets/Scripts/Managers/IntroMenuManager.cs
+++ Assets/Scripts/Managers/IntroMenuManager.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IntroMenuManager : MonoBehaviour {
+public class IntroMenuManager : MonoBehaviour 
+{
+	public void NewGameButton()
+	{
+		Application.LoadLevel("ZenGarden");
+	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
