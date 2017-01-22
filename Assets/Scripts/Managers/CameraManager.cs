09bccc3f9d012e4e496a3aca0f6980a68f32db38
patchthesock <stephencallaghan91@gmail.com>
2017-01-22 12:42:27 +0000

    No Camera Pan

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -47,7 +47,7 @@ namespace Assets
             //        _dragOrigin = Input.mousePosition;
             //        Debug.Log("fired");
             //    }
-                
+
             //    var pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - _dragOrigin);
             //    var move = new Vector2(pos.x, pos.y) * -1; //* FlySpeed * -1;
             //    //TheCamera.transform.Translate(new Vector3(TheCamera.transform.forward.x + pos.x, 0, TheCamera.transform.right.z + pos.y) , Space.World);
@@ -55,10 +55,10 @@ namespace Assets
             //    TheCamera.transform.position = new Vector3(
             //        TheCamera.transform.position.x + move.x,
             //        TheCamera.transform.position.y,
-            //        TheCamera.transform.position.z + move.y); 
+            //        TheCamera.transform.position.z + move.y);
             //}
 
-            
+
 
             ////
             //use shift to speed up flight
