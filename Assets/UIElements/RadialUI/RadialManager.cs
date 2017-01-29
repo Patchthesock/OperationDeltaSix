3d72a9e500b0534b8dbaa6dc3041d07a26f886d2
Conor Brien <conorbrien@gmail.com>
2017-01-29 23:58:41 +0000

    Menu Testing, working to a degree

---
+++ Assets/UIElements/RadialUI/RadialManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialManager : MonoBehaviour
+{
+	public GameObject DominoesInventory;
+	public GameObject PropsInventory;
+	public GameObject MainUI;
+	public bool InventoryOpen;
+	
+	public GameObject ActiveInventory;
+	public Transform GrandChild;
+	
+	public void DominoesButton()
+	{
+		DominoesInventory.SetActive(true);
+		ActiveInventory = DominoesInventory;
+		foreach(Transform child in MainUI.transform)
+		{
+			GrandChild = child.transform.GetChild(0);
+			GrandChild.GetComponent<Button>().interactable = false;
+		}
+		InventoryOpen = true;
+	}
+	
+	public void PropsButton()
+	{
+		PropsInventory.SetActive(true);
+		ActiveInventory = PropsInventory;
+		foreach(Transform child in MainUI.transform)
+		{
+			child.GetComponent<Button>().interactable = false;
+		}
+		InventoryOpen = true;
+	}
+
+	public void ExitButton()
+	{
+		foreach(Transform child in MainUI.transform)
+		{
+			GrandChild = child.transform.GetChild(0);
+			GrandChild.GetComponent<Button>().interactable = true;
+			InventoryOpen = false;
+			ActiveInventory.SetActive(false);
+		}
+	}
+	
+	void Update()
+	{
+		if (InventoryOpen == true)
+		{
+			if(Input.GetKeyUp(KeyCode.Escape))
+			{
+				foreach(Transform child in MainUI.transform)
+				{
+					child.GetComponent<Button>().interactable = true;
+					InventoryOpen = false;
+					ActiveInventory.SetActive(false);
+				}
+			}
+		}
+	}	
+}
