41a4a38db940f9255d2b317ee2a7052c82c39e32
DavidMulhall <Darkne55>
2017-04-01 16:20:18 +0100

    Water feature, lux running water shader and new State of Play branch

---
+++ Assets/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public int Xspeed;
+	public int Yspeed;
+	public int Zspeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (new Vector3(Xspeed,Yspeed,Zspeed)* Time.deltaTime);
+	}
+}
