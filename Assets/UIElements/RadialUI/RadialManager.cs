632ba2627074f5aca7ecf2ff3af27ce4a1d2b8fd
Conor Brien <conorbrien@gmail.com>
2017-04-03 14:11:21 +0100

    Radial Menu Interface

---
+++ Assets/UIElements/RadialUI/RadialManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialManager : MonoBehaviour
+{
+	public GameObject DominoesInventory;
+	public GameObject PropsInventory;
+	public GameObject MainUI;
+	public bool InventoryOpen;
+	
+	public GameObject ActiveInventory;
+	public Transform GrandChild;
+	
+	public void DominoesButton()
+	{
+		DominoesInventory.SetActive(true);
+		ActiveInventory = DominoesInventory;
+		/* Controls the MainUI elements and turns them off on selection
+		foreach(Transform child in MainUI.transform)
+		{
+			GrandChild = child.transform.GetChild(0);
+			GrandChild.GetComponent<Button>().interactable = false;
+		}
+		*/
+		InventoryOpen = true;
+	}
+	
+	public void PropsButton()
+	{
+		PropsInventory.SetActive(true);
+		ActiveInventory = PropsInventory;
+		foreach(Transform child in MainUI.transform)
+		{
+			child.GetComponent<Button>().interactable = false;
+		}
+		InventoryOpen = true;
+	}
+
+	public void ExitButton()
+	{
+		foreach(Transform child in MainUI.transform)
+		{
+			//GrandChild = child.transform.GetChild(0);
+			//GrandChild.GetComponent<Button>().interactable = true;
+			InventoryOpen = false;
+			ActiveInventory.SetActive(false);
+		}
+	}
+	
+	void Update()
+	{
+		if (InventoryOpen == true)
+		{
+			if(Input.GetKeyUp(KeyCode.Escape))
+			{
+				foreach(Transform child in MainUI.transform)
+				{
+					child.GetComponent<Button>().interactable = true;
+					InventoryOpen = false;
+					ActiveInventory.SetActive(false);
+				}
+			}
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			MainUI.transform.position = Input.mousePosition;
+			MainUI.SetActive(true);
+		}
+	}	
+}
