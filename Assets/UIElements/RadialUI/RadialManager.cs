6df7d66ed39c3c32323564d8810505f93f2199f2
patchthesock <stephencallaghan91@gmail.com>
2017-07-09 11:29:59 +0100

    Light Mapping for Intro

--- Assets/UIElements/RadialUI/RadialManager.cs
+++ Assets/UIElements/RadialUI/RadialManager.cs
@@ -1,83 +1,82 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class RadialManager : MonoBehaviour
+namespace Assets.UIElements.RadialUI
 {
-	public GameObject DominoesInventory;
-	public GameObject PropsInventory;
-	public GameObject MainUI;
-	public bool InventoryOpen;
+    public class RadialManager : MonoBehaviour
+    {
+        public GameObject DominoesInventory;
+        public GameObject PropsInventory;
+        public GameObject MainUI;
+        public bool InventoryOpen;
 	
-	public GameObject ActiveInventory;
-	public Transform GrandChild;
+        public GameObject ActiveInventory;
+        public Transform GrandChild;
 	
-	public void DominoesButton()
-	{
-		DominoesInventory.SetActive(true);
-		PropsInventory.SetActive(false);
-		ActiveInventory = DominoesInventory;
-		/* Controls the MainUI elements and turns them off on selection
+        public void DominoesButton()
+        {
+            DominoesInventory.SetActive(true);
+            PropsInventory.SetActive(false);
+            ActiveInventory = DominoesInventory;
+            /* Controls the MainUI elements and turns them off on selection
 		foreach(Transform child in MainUI.transform)
 		{
 			GrandChild = child.transform.GetChild(0);
 			GrandChild.GetComponent<Button>().interactable = false;
 		}
 		*/
-		InventoryOpen = true;
-	}
+            InventoryOpen = true;
+        }
 	
-	public void PropsButton()
-	{
-		PropsInventory.SetActive(true);
-		DominoesInventory.SetActive(false);
-		ActiveInventory = PropsInventory;
-		/* foreach(Transform child in MainUI.transform)
+        public void PropsButton()
+        {
+            PropsInventory.SetActive(true);
+            DominoesInventory.SetActive(false);
+            ActiveInventory = PropsInventory;
+            /* foreach(Transform child in MainUI.transform)
 		{
 			child.GetComponent<Button>().interactable = false;
 		}
 		*/
-		InventoryOpen = true;
-	}
+            InventoryOpen = true;
+        }
 
-	public void ExitButton()
-	{
-		foreach(Transform child in MainUI.transform)
-		{
-			//GrandChild = child.transform.GetChild(0);
-			//GrandChild.GetComponent<Button>().interactable = true;
-			InventoryOpen = false;
-			ActiveInventory.SetActive(false);
-			MainUI.SetActive(false);
-		}
-	}
+        public void ExitButton()
+        {
+            foreach(Transform child in MainUI.transform)
+            {
+                //GrandChild = child.transform.GetChild(0);
+                //GrandChild.GetComponent<Button>().interactable = true;
+                InventoryOpen = false;
+                ActiveInventory.SetActive(false);
+                MainUI.SetActive(false);
+            }
+        }
 	
-	public void ExitGame()
-	{
-		Application.Quit();
-	}
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
 	
-	void Update()
-	{
-		if (InventoryOpen == true)
-		{
-			if(Input.GetKeyUp(KeyCode.Escape))
-			{
-				foreach(Transform child in MainUI.transform)
-				{
-					child.GetComponent<Button>().interactable = true;
-					InventoryOpen = false;
-					ActiveInventory.SetActive(false);
-				}
-			}
-		}
-		
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			MainUI.transform.position = Input.mousePosition;
-			MainUI.SetActive(true);
-			InventoryOpen = true;
-		}
-	}	
+        private void Update()
+        {
+            if (InventoryOpen == true)
+            {
+                if(Input.GetKeyUp(KeyCode.Escape))
+                {
+                    foreach(Transform child in MainUI.transform)
+                    {
+                        child.GetComponent<Button>().interactable = true;
+                        InventoryOpen = false;
+                        ActiveInventory.SetActive(false);
+                    }
+                }
+            }
+
+            if (!Input.GetKeyDown(KeyCode.Space)) return;
+            MainUI.transform.position = Input.mousePosition;
+            MainUI.SetActive(true);
+            InventoryOpen = true;
+        }	
+    }
 }
--- Assets/UIElements/RadialUI/RaycastMask.cs
+++ Assets/UIElements/RadialUI/RaycastMask.cs
@@ -1,41 +1,44 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
- 
-[RequireComponent(typeof(RectTransform))]
-[RequireComponent(typeof(Image))]
-public class RaycastMask : MonoBehaviour, ICanvasRaycastFilter
+
+namespace Assets.UIElements.RadialUI
 {
-    private Sprite _sprite;
+    [RequireComponent(typeof(RectTransform))]
+    [RequireComponent(typeof(Image))]
+    public class RaycastMask : MonoBehaviour, ICanvasRaycastFilter
+    {
+        private Sprite _sprite;
  
-    void Start ()
-    {
-        _sprite = GetComponent<Image>().sprite;
-    }
-   
-    public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
-    {
-        var rectTransform = (RectTransform)transform;
-        Vector2 local;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform) transform, sp, eventCamera, out local);
-        // normalize local coordinates
-        var normalized = new Vector2(
-            (local.x + rectTransform.pivot.x*rectTransform.rect.width)/rectTransform.rect.width,
-            (local.y + rectTransform.pivot.y*rectTransform.rect.height)/rectTransform.rect.width);
-        // convert to texture space
-        var rect = _sprite.textureRect;
-        var x = Mathf.FloorToInt(rect.x + rect.width * normalized.x);
-        var y = Mathf.FloorToInt(rect.y + rect.height * normalized.y);
-        // destroy component if texture import settings are wrong
-        try
+        private void Start ()
         {
-            return _sprite.texture.GetPixel(x,y).a > 0;
+            _sprite = GetComponent<Image>().sprite;
         }
-        catch (UnityException e)
+   
+        public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
         {
-            Debug.LogError("Mask texture not readable, set your sprite to Texture Type 'Advanced' and check 'Read/Write Enabled'");
-            Debug.LogError(e.Message);
-            Destroy(this);
-            return false;
+            var rectTransform = (RectTransform)transform;
+            Vector2 local;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform) transform, sp, eventCamera, out local);
+            // normalize local coordinates
+            var normalized = new Vector2(
+                (local.x + rectTransform.pivot.x*rectTransform.rect.width)/rectTransform.rect.width,
+                (local.y + rectTransform.pivot.y*rectTransform.rect.height)/rectTransform.rect.width);
+            // convert to texture space
+            var rect = _sprite.textureRect;
+            var x = Mathf.FloorToInt(rect.x + rect.width * normalized.x);
+            var y = Mathf.FloorToInt(rect.y + rect.height * normalized.y);
+            // destroy component if texture import settings are wrong
+            try
+            {
+                return _sprite.texture.GetPixel(x,y).a > 0;
+            }
+            catch (UnityException e)
+            {
+                Debug.LogError("Mask texture not readable, set your sprite to Texture Type 'Advanced' and check 'Read/Write Enabled'");
+                Debug.LogError(e.Message);
+                Destroy(this);
+                return false;
+            }
         }
     }
 }
